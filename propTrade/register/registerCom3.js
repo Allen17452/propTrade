e4153e8b3974b085d8d050bedc9719b6bd0e8c35
Allen17452 <allen.tang@mc900.com>
2024-07-19 10:03:27 +0800

    Delete propTrade directory

--- propTrade/register/registerCom3.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("optionA").addEventListener("click", function() {
-  document.getElementById("optionA").style.display = "none";
-  document.getElementById("optionB").style.display = "block";
-});
-document.getElementById("optionB").addEventListener("click", function() {
-  document.getElementById("optionB").style.display = "none";
-  document.getElementById("optionA").style.display = "block";
-});
